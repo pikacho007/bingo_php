55ba587bcaf7f9287a5ca4db825f3183d1ebfa30
Komiya Chizuko <seven.met0715@gmail.com>
2020-02-05 15:18:24 +0900

    Delete no needed code

--- index.php
+++ index.php
@@ -6,30 +6,6 @@ require_once(__DIR__.'/Bingo.php');
 $bingo = new \MyApp\Bingo();
 $nums = $bingo->create();
 
-/*
-B: $nums[0] 1-15
-B: $nums[1] 16-30
-・・
-O:nums[4]
-
-$nums[$i] $i * 15 + 1 ~ $i * 15 + 15
-*/
-
-$nums = [];
-
-for ($i = 0; $i < 5; $i++) {
-  $col = range($i * 15 + 1, $i * 15 + 15);
-
-  shuffle($col);
-  $nums[$i] = array_slice($col,0 ,5);
-}
-
-$nums[2][2] = "FREE";
-
-// var_dump($nums);
-// exit;
-
-
 
 ?>
 
